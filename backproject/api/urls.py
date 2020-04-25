2dea242c0faf45a71ce93a576a5290a5b4fe64b0
Alim <alim.atabay117@gmail.com>
2020-04-25 21:29:14 +0600

    comment

--- backproject/api/urls.py
+++ backproject/api/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     # path('logout/', views.logout),
     path('categories/', views.CategoryList.as_view()),
     path('categories/<int:pk>/', views.CategoryDetail.as_view()),
+    path('products/', views.ProductList.as_view()),
+    path('products/<int:pk>/', views.ProductDetail.as_view()),
     # path('categories/<int:pk>/products/', views.CategoryProductsAPIView.as_view()),
     # path('categories/products/', views.CategoryProductCreateAPIView.as_view())
 ]
--- backproject/api/views/cbv.py
+++ backproject/api/views/cbv.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from api.models import Category
-from api.serializers import CategorySerializer2
+from api.models import Category, Product
+from api.serializers import CategorySerializer2, ProductSerializer2
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -47,4 +47,44 @@ class CategoryDetail(APIView):
     def delete(self, request, pk):
         category = self.get_object(pk)
         category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ProductList(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer2(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = ProductSerializer2(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class ProductDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(id=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer2(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer2(instance=product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        product = self.get_object(pk)
+        product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
